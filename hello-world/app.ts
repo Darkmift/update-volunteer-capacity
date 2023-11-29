e6038be20801a16f11686677c6f763b06417a5cd
aek <darkmift@gmail.com>
2023-11-29 16:56:17 -0300

    [main] Add functionality to update column value for item in board

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,5 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import logger from './utils/logger-winston';
+import tryParse from './utils/tryparse';
+import { MondayUpdateItemEvent, UpdateColumnValueVariables } from './types';
+import { ASSIGNEES_COLUMN_ID, CAPACITY_COLUMN_ID } from './config/consts';
+import makeGQLRequest from './utils/graphQlRequestClient';
+import { UPDATE_COLUMN_VALUE_FOR_ITEM_IN_BOARD } from './utils/queries';
 
 /**
  *
@@ -14,11 +19,52 @@ import logger from './utils/logger-winston';
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     logger.info('hello world', { event });
     try {
-        return {
+        /**
+         * we extract the monday event
+         */
+
+        const response = {
             statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
+            body: {
+                message: 'fn run started',
+                challenge: 'no challenge found',
+            },
+        };
+
+        const mondayEvent = tryParse(event.body) as { event: MondayUpdateItemEvent };
+        const {
+            event: { value, columnId, pulseId, boardId },
+        } = mondayEvent;
+
+        logger.info('values', { value, columnId, mondayEvent });
+
+        if (columnId !== ASSIGNEES_COLUMN_ID) {
+            response.body.message = `not the ${ASSIGNEES_COLUMN_ID} column`;
+            response.body.challenge = mondayEvent.event.challenge || response.body.challenge;
+
+            return {
+                ...response,
+                body: JSON.stringify(response.body),
+            };
+        }
+
+        // we count the number of linked pulses
+        const assignedItemsCount = value.linkedPulseIds?.length || 0;
+        logger.log('🚀 ~ file: app.ts:51 ~ lambdaHandler ~ pulseCount:', { value, pulseCount: assignedItemsCount });
+
+        const variables: UpdateColumnValueVariables = {
+            volunteerId: pulseId,
+            boardId: boardId,
+            columnId: CAPACITY_COLUMN_ID,
+            value: assignedItemsCount.toString(),
+        };
+
+        await makeGQLRequest(UPDATE_COLUMN_VALUE_FOR_ITEM_IN_BOARD, variables);
+
+        response.body.message = `assignedItemsCount count is ${assignedItemsCount}`;
+        return {
+            ...response,
+            body: JSON.stringify(response.body),
         };
     } catch (err) {
         console.log(err);
--- hello-world/tests/unit/test-handler.test.ts
+++ hello-world/tests/unit/test-handler.test.ts
@@ -1,17 +1,52 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
 import { lambdaHandler } from '../../app';
-import { expect, describe, it } from '@jest/globals';
-import mockEvent from '../../mocks/event';
+import { expect, describe, it, beforeEach } from '@jest/globals';
+import mockEvent, { mockMondayEvent } from '../../mocks/event';
+import { ASSIGNEES_COLUMN_ID } from '../../config/consts';
+
+let mockEventClone = structuredClone(mockEvent);
+let mockMondayEventClone = structuredClone(mockMondayEvent);
 
 describe('Unit test for app handler', function () {
+    beforeEach(() => {
+        // Reset mockEventClone to a fresh clone of mockEvent before each test
+        mockEventClone = structuredClone(mockEvent);
+        mockMondayEventClone = structuredClone(mockMondayEvent);
+    });
+
     it('verifies successful response', async () => {
-        const result: APIGatewayProxyResult = await lambdaHandler(mockEvent);
+        const result: APIGatewayProxyResult = await lambdaHandler(mockEventClone);
 
         expect(result.statusCode).toEqual(200);
-        expect(result.body).toEqual(
-            JSON.stringify({
-                message: 'hello world',
-            }),
-        );
+    });
+
+    it('verifies successful response with wrong column ID', async () => {
+        mockMondayEventClone.event.columnId = 'not the right column id';
+
+        mockEventClone.body = JSON.stringify(mockMondayEventClone);
+
+        const result: APIGatewayProxyResult = await lambdaHandler(mockEventClone);
+        const { message } = JSON.parse(result.body);
+
+        expect(result.statusCode).toEqual(200);
+        expect(message.includes(`not the ${ASSIGNEES_COLUMN_ID} column`)).toBeTruthy();
+    });
+
+    it('verifies successful response with modified column ID', async () => {
+        mockMondayEventClone.event.columnId = ASSIGNEES_COLUMN_ID;
+        mockMondayEventClone.event.value.linkedPulseIds = [
+            { linkedPulseId: 78364654 },
+            { linkedPulseId: 78364655 },
+            { linkedPulseId: 78364656 },
+            { linkedPulseId: 78364657 },
+            { linkedPulseId: 78364658 },
+        ];
+        mockEventClone.body = JSON.stringify(mockMondayEventClone);
+
+        const result: APIGatewayProxyResult = await lambdaHandler(mockEventClone);
+        const { message } = JSON.parse(result.body);
+
+        expect(result.statusCode).toEqual(200);
+        expect(message).toBe(`assignedItemsCount count is 5`);
     });
 });
