734b03baa66ee923fce6c63ff1b787f85967be51
aek <darkmift@gmail.com>
2023-11-29 16:56:05 -0300

    [main] Refactor tryParse function to support generic type parsing

--- hello-world/utils/tryparse.ts
+++ hello-world/utils/tryparse.ts
@@ -1,10 +1,10 @@
-const tryParse = (input: unknown): unknown => {
+const tryParse = <T>(input: unknown): T | unknown => {
     if (!input) {
         return input;
     }
     try {
-        // Attempt to parse the input as JSON
-        return JSON.parse(input as string);
+        // Attempt to parse the input as JSON and cast it to the type T
+        return JSON.parse(input as string) as T;
     } catch (error) {
         // If parsing fails, return the original input
         return input;
